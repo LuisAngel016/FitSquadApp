e02f33fae3131626e891ec2038dc315f89639939
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-02-27 17:23:18 -0500

    FitSquadPage Modificado

--- src/fitSquad/pages/FitSquadPage.jsx
+++ src/fitSquad/pages/FitSquadPage.jsx
@@ -1,9 +1,24 @@
+import { NavBar } from "../components/NavBar";
+import { SideBar } from "../components/SideBar"
 
+
+// const drawerWidth = 240;
 
 export const FitSquadPage = () => {
   return (
-    <>
-      <h1>FitSquadPage</h1>
-    </>
+    <section className="flex">
+
+        <SideBar /* drawerWidth={ drawerWidth } */ />
+
+        <section className="m-10">
+          <NavBar /* drawerWidth={ drawerWidth } */ />
+
+
+          <h1>FitSquadPage</h1>
+
+        </section>
+
+
+    </section>
   )
 }
