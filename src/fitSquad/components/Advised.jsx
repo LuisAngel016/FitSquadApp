c22013034cce58c1fb84508ef8eff3d971423f9e
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-04-27 11:55:36 -0500

    Advised modificado en /components

--- src/fitSquad/components/Advised.jsx
+++ src/fitSquad/components/Advised.jsx
@@ -1,18 +1,21 @@
 
 
-export const Advised = ({ img, name, estado, objetivo, peso }) => {
+export const Advised = ({ img, user }) => {
+
+  const { name, estado, objetivo, peso } = user;
+
   return (
     <tbody>
       <tr className="border-b border-gray-500">
-        <td class=" px-4 py-2 flex items-center gap-2">
+        <td className=" px-4 py-2 flex items-center gap-2">
           <figure className='w-12'>
             <img className='rounded-full ' src={ img } alt="" />
           </figure>
           { name }
         </td>
-        <td class="text-center  px-4 py-2">{ estado }</td>
-        <td class="text-center  px-4 py-2">{ objetivo }</td>
-        <td class="text-center  px-4 py-2">{ peso }</td>
+        <td className="text-center  px-4 py-2">{ estado }</td>
+        <td className="text-center  px-4 py-2">{ objetivo }</td>
+        <td className="text-center  px-4 py-2">{ peso }</td>
       </tr>
     </tbody>
   )
