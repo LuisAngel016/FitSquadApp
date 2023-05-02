56992446d9f902d46c8a4d1eccb4a2d2d4aa944d
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-05-01 19:39:20 -0500

    LoginPage modificado

--- src/auth/Page/LoginPage.jsx
+++ src/auth/Page/LoginPage.jsx
@@ -1,34 +1,13 @@
 import { NavLink } from 'react-router-dom';
-import imgFit from '../../assets/img/imgLogin.jpg';
-import { useEffect, useMemo, useState } from 'react';
 
+import { useFormIU } from '../hooks';
+import imgFit from '../../assets/img/imgLogin.jpg';
 import './LoginPage.css';
+
 
 export const LoginPage = () => {
 
-  const [activeEmail, setActiveEmail] = useState(false);
-  const [activePassword, setActivePassword] = useState(false);
-
-  const handleClickInputEmail = () => {
-    setActiveEmail(true);
-  };
-
-  const handleClickInputPassword = () => {
-    setActivePassword(true);
-  };
-
-  const focusInput = function ( event ) {
-    event.target.parentElement.children[1].className = "label active";
-    event.target.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
-  };
-
-  const blurInput = function (event) {
-    if (event.target.value <= 0) {
-      event.target.parentElement.children[1].className = "label";
-      event.target.parentElement.children[0].className = this.parentElement.children[0].className + " error";
-    }
-  };
-  
+  const { activeEmail, activePassword, handleClickInputEmail, handleClickInputPassword, focusInput, blurInput } = useFormIU();
 
   return (
     <>
@@ -43,11 +22,11 @@ export const LoginPage = () => {
           <hr />
         <section className='bg-white flex-col justify-center'>
           <h1 className="text-3xl text-secondary font-extrabold mb-14 font-FitSquad text-center">FitSquad</h1>
-          <div class="contenedor-formulario">
-            <div class="wrap">
-              <form action="" class="formulario font-FitSquad">
+          <div className="contenedor-formulario">
+            <div className="wrap">
+              <form action="" className="formulario font-FitSquad">
                 <div>
-                  <div class="input-group">
+                  <div className="input-group">
                     <input 
                       type="email" 
                       id="correo" 
@@ -57,9 +36,9 @@ export const LoginPage = () => {
                       onBlur={ blurInput }
                       onFocus={ focusInput }
                     />
-                    <label class="label"  for="correo">Correo electronico</label>
+                    <label className="label"  for="correo">Correo electronico</label>
                   </div>
-                  <div class="input-group">
+                  <div className="input-group">
                     <input 
                       type="password" 
                       id="pass" 
@@ -69,7 +48,7 @@ export const LoginPage = () => {
                       onBlur={ blurInput }
                       onFocus={ focusInput }
                     />
-                    <label class="label" for="pass">Contraseña</label>
+                    <label className="label" for="pass">Contraseña</label>
                   </div>
                 </div>
                 <div className='flex mt-2 justify-between'>
@@ -83,7 +62,7 @@ export const LoginPage = () => {
                   </NavLink>
                 </div>
                 <button
-                  className='w-full mt-12 p-2 bg-primary text-xs text-center text-primary font-FitSquad rounded-lg  hover:bg-violet-700 hover:file:text-violet-200'
+                  className='w-full mt-12 p-2 bg-primary text-xs text-center text-primary font-FitSquad rounded-lg  hover:bg-violet-700 hover:text-violet-200'
                   type='submit'
                 >
                   Iniciar sesión
