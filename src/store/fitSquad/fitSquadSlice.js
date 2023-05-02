ab09999bac76af1f96ac32fc7fb797cc92c26de7
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-05-01 19:35:55 -0500

    fitSquadSlice modificado

--- src/store/fitSquad/fitSquadSlice.js
+++ src/store/fitSquad/fitSquadSlice.js
@@ -3,15 +3,53 @@ import { createSlice } from '@reduxjs/toolkit';
 export const fitSquadSlice = createSlice({
     name: 'fitSquad',
     initialState: {
-        counter: 10,
+        isLoadingFile: false,
+        users: [
+
+        ],
+        selectUser: null,
+        errorMessage: null,
     },
     reducers: {
-        increment: (state, /* action */ ) => {
-            
-        }
-    }
-
+        // onStartLoadingFile: (state) => {
+        //     state.isLoadingFile = true;
+        // },
+        // onStopLoadingFile: (state) => {
+        //     state.isLoadingFile = false;
+        // },
+        onSetIsloadingFile: (state, { payload }) => {
+            state.isLoadingFile = {
+                name: payload.name,
+                type: payload.type,
+                size: payload.size,
+            };
+        },          
+        onClearIsLoadingFile: (state) => {
+            state.isLoadingFile = false;
+        },
+        onSetError: (state, { payload }) => {
+            state.isLoadingFile = null;
+            state.errorMessage = payload;
+        },
+        onSetSelectUser: ( state) => {
+            state.selectUser = true;
+        },
+        onClearSelectUser: (state, { payload } ) => {
+            state.selectUser = false;
+            state.errorMessage = payload;
+        },          
+    },
+    serializeReducers: true, // elimina los valores no serializables automáticamente
 });
 
+
 // Action creators are generated for each case reducer function
-export const { increment } = fitSquadSlice.actions;
+export const {
+    onClearIsLoadingFile,
+    onSetIsloadingFile,
+    onSetError,
+    onSetSelectUser,
+    onClearSelectUser,
+    // onStartLoadingFile,
+    // onStopLoadingFile,
+} = fitSquadSlice.actions;
