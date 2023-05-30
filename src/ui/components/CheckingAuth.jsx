97b32706c500f37e4d41a6c9800645bd3e383213
Luis Angel Bustamante <luisbustamante1571@gmail.com>
2023-05-30 10:20:04 -0500

    Loader creado

---
+++ src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,11 @@
+import './Loader.css';
+
+export const CheckingAuth = () => {
+  return (
+    <div className="loading">
+      <div className="lding">
+        <div></div>
+      </div>
+    </div>
+  );
+}
